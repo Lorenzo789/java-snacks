4ff2341e60a5278b2b7d4ce9c38b414485f37695
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-11-16 10:29:19 +0100

    second commit

--- src/org/generation/italy/Snack1.java
+++ src/org/generation/italy/Snack1.java
@@ -1,8 +1,29 @@
 package org.generation.italy;
 
+import java.util.Scanner;
+
+//Inserisci un numero, se è pari stampa il numero,
+//se è dispari stampa il numero successivo.
+
 public class Snack1 {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Inerisci un numero: ");
+		int numberQuestion = sc.nextInt();
+		
+		sc.close();
+		
+		if (numberQuestion % 2 == 0) {
+			
+			System.out.println(numberQuestion);
+			
+		} else {
+			
+			System.out.println(numberQuestion + 1);
+			
+		}
 	}
 }
---
+++ src/org/generation/italy/Snack2.java
@@ -0,0 +1,36 @@
+package org.generation.italy;
+
+import java.util.Random;
+import java.util.jar.Attributes.Name;
+
+public class Snack2 {
+	
+	/*
+	 * Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi,
+	 *  e da queste vuole mostrare a video una falsa lista di invitati con nome e cognome.
+	 */
+	
+	public static void main(String[] args) {
+		
+		Random rnd = new Random();
+		
+		String[] names = {"Orion", "Tyrion", "Azazel"};
+		String[] surnames = {"Drycha", "Skrag", "Grimgor"};
+		
+		String[] fullNames = new String[3];
+		
+		for (int i = 0; i < names.length; i++) {
+			
+			String name = names[i];
+			
+			int surnameIndex = rnd.nextInt(surnames.length);
+			String surname = surnames[surnameIndex];
+			
+			String fullName = name + " " + surname;
+			fullNames[i] = fullName;
+			
+			System.out.println(fullName);
+		}
+	}
+	
+}
